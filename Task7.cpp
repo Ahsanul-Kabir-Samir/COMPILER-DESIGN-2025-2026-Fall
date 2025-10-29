a47b52349ec45ac2ccc4c07309d14c712b24c09f
Ahsanul-Kabir-Samir <kabir2k4@gmail.com>
2025-10-29 14:50:07 +0600

    Add files via upload

---
+++ Task7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string firstName, lastName, fullName;
+
+  cout << "Write your first name: ";
+
+     cin >> firstName;
+
+       cout << "Write your last name: ";
+
+     cin >> lastName;
+
+  fullName = firstName + " " + lastName;
+
+      cout << "Full name = " << fullName << endl;
+
+
+
+
+
+    return 0;
+}
+
